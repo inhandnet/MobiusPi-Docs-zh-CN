22278827e271939d539cbb6b7d84e920894ccdca
zhangning <zhangning@inhand.com.cn>
2019-12-27 17:59:46 +0800

    优化展示效果

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ pygments_style = None
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = "_static/IG500.png"
+html_logo = "_static/logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
